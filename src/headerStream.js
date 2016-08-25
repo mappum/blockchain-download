0be1783d788cab869b80792c706fd473d670997c
Matt Bell <mappum@gmail.com>
2016-08-24 20:34:19 -0700

    HeaderStream: extract header callback

--- src/headerStream.js
+++ src/headerStream.js
@@ -15,10 +15,12 @@ var HeaderStream = module.exports = function (peers, opts) {
   this.peers = peers
   this.timeout = opts.timeout
   this.stop = opts.stop
-  this.getting = false
+  this.lookAhead = opts.lookAhead != null ? opts.lookAhead : true
   this.done = false
   this.reachedTip = false
   this.lastLocator = null
+  this.lookAheadHash = null
+  this.lookAheadHeaders = null
   if (opts.endOnTip) {
     this.once('tip', () => this.end())
   }
@@ -32,42 +34,45 @@ HeaderStream.prototype._error = function (err) {
 HeaderStream.prototype._transform = function (locator, enc, cb) {
   this.lastLocator = locator
   if (this.reachedTip) return cb(null)
-  this._getHeaders(locator, cb)
+  this._getHeaders(locator, (err, headers, peer) => {
+    if (err) return this._error(err)
+    this._onHeaders(headers, peer, cb)
+  })
 }
 
 HeaderStream.prototype._getHeaders = function (locator, peer, cb) {
-  if (this.getting || this.done) return
+  if (this.done) return
   if (typeof peer === 'function') {
     cb = peer
     peer = null
   }
-  if (!peer) peer = this.peers
-  this.getting = true
+  peer = peer || this.peers
   peer.getHeaders(locator, {
     stop: this.stop,
     timeout: this.timeout
-  }, (err, headers, peer) => {
-    if (this.done) return cb(null)
-    if (err) return this._error(err)
-    this.getting = false
-    if (headers.length === 0) {
-      this._onTip(peer)
-      if (cb) cb(null)
-      return
-    }
-    headers.peer = peer
-    this.push(headers)
-    if (headers.length < 2000) {
-      this._onTip(peer)
-      if (cb) cb(null)
-      return
-    }
-    if (this.stop &&
-    headers[headers.length - 1].getHash().compare(this.stop) === 0) {
-      this.end()
-    }
+  }, cb)
+}
+
+HeaderStream.prototype._onHeaders = function (headers, peer, cb) {
+  if (this.done) return cb(null)
+  this.getting = false
+  if (headers.length === 0) {
+    this._onTip(peer)
     if (cb) cb(null)
-  })
+    return
+  }
+  headers.peer = peer
+  this.push(headers)
+  if (headers.length < 2000) {
+    this._onTip(peer)
+    if (cb) cb(null)
+    return
+  }
+  if (this.stop &&
+  headers[headers.length - 1].getHash().compare(this.stop) === 0) {
+    this.end()
+  }
+  if (cb) cb(null)
 }
 
 HeaderStream.prototype.end = function () {
@@ -94,7 +99,10 @@ HeaderStream.prototype._subscribeToInvs = function () {
       }
       lastSeen.push(item.hash)
       if (lastSeen.length > 8) lastSeen.shift()
-      this._getHeaders(this.lastLocator, peer)
+      this._getHeaders(this.lastLocator, peer, (err, headers, peer) => {
+        if (err) return this._error(err)
+        this._onHeaders(headers, peer, () => {})
+      })
     }
   })
 }
